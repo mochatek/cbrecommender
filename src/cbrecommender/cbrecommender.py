5264b64303721f70ec611e85eb8517256a22803c
mochatek <akashsabu@ymail.com>
2022-07-30 08:57:14 +0530

    feat: change names in core library

--- src/cbrecommender/cbrecommender.py
+++ src/cbrecommender/cbrecommender.py
@@ -20,82 +20,87 @@ class CBRecommender:
         """ Getter for user_profile """
         return self.__user_profile
 
+    @staticmethod
+    def clean_and_encode(features: DataFrame) -> DataFrame:
+        """ Helper function to clean and One-Hot Encode the feature """
+        return (features[feature_name].apply(
+            remove_spaces).str.get_dummies(sep=',') for feature_name in features)
+
     @ensure_type([DataFrame], instance_method=True)
-    def encode_features(self, features: DataFrame) -> DataFrame:
+    def create_item_profile(self, features: DataFrame) -> DataFrame:
         """
-        One-Hot-Encode the features used for recommendation
+        Creates a profile for every item based on the supplied features
 
         Args:
-            `features` (DataFrame): Features to encode
+            `features` (DataFrame): Relevant attributes of the item, based on which the item profile is created
                 Example: Movie genres
 
         Returns:
-            `encoded_features` (DataFrame): One-Hot-Encoded form of the features
+            `item_profiles` (DataFrame): One-Hot-Encoded form of the features
         """
 
-        encoded_features = concat((features[feature_name].apply(
-            remove_spaces).str.get_dummies(sep=',') for feature_name in features), axis=1)
-
-        return encoded_features
+        item_profiles = concat(
+            CBRecommender.clean_and_encode(features), axis=1)
+        return item_profiles
 
     @ensure_type([DataFrame, list], instance_method=True)
-    def fit(self, train_features: DataFrame, scores: list) -> DataFrame:
+    def fit(self, train_item_profiles: DataFrame, scores: list) -> DataFrame:
         """
-        Builds the user_profile by extracting user's preference from the train_features and scores.
+        Builds the user_profile by extracting user's preference from the train_item_profiles and associated scores.
 
         Args:
-            `train_features` (DataFrame): Features for training the model. It should be One-Hot-Encoded.
-                Example: Genres of watched movies
+            `train_item_profiles` (DataFrame): item profiles used for building the user_profile.
+                Example: item_profiles of watched movies
 
-            `scores` (float): Score associated to each item in train_features
+            `scores` (float): Score associated to each item in train_item_profiles
                 Example: User rating for each watched movie
 
         Returns:
             `user_profile` (DataFrame): The preference model built for the user, which will be used for recommending.
         """
 
-        if len(train_features.index) != len(scores):
+        if len(train_item_profiles.index) != len(scores):
             raise ValueError(
-                f'train_features and scores should be equal in length, got {len(train_features.index)} and {len(scores)} instead.')
+                f'train_item_profiles and scores should be equal in length, got {len(train_item_profiles.index)} and {len(scores)} instead.')
 
-        feature_matrix = train_features.values
+        feature_matrix = train_item_profiles.values
         score_matrix = array(scores, ndmin=2)
         profile_matrix = score_matrix.dot(feature_matrix)
         normalized_profile_matrix = profile_matrix / profile_matrix.sum()
 
         self.__user_profile = DataFrame(
-            normalized_profile_matrix, columns=train_features.columns)
+            normalized_profile_matrix, columns=train_item_profiles.columns)
         return self.user_profile
 
     @ensure_type([DataFrame, DataFrame, float, int], instance_method=True)
-    def recommend(self, items: DataFrame, test_features: DataFrame, threshold_score: float = 7.5, limit: int = -1) -> DataFrame:
+    def recommend(self, test_items: DataFrame, test_item_profiles: DataFrame, min_score: float = 7.5, limit: int = -1) -> DataFrame:
         """
         Recommend items based on user_profile
 
         Args:
-            `items` Items from which recommendation is needed
+            `test_items` (DataFrame) Items from which recommendation is needed
 
-            `test_features` (DataFrame): One-Hot-Encoded features of the items.
-                Example: Genres of unwatched movies
+            `test_item_profiles` (DataFrame): Profile of each item in test_items.
+                Example: item_profiles of unwatched movies
 
-            `threshold_score` (float): Minimum score used as threshold value in recommending items. Default is 7.5
+            `min_score` (float): Minimum score used as threshold value in recommending items. Default is 7.5
 
             `limit` (int): Number of items to recommend
 
         Returns:
-            `recommendations` (DataFrame): DataFrame with item and predicted score.
+            `recommendations` (DataFrame): DataFrame with the test_item and predicted score.
         """
 
-        if set(test_features.columns) != set(self.user_profile.columns):
+        if set(test_item_profiles.columns) != set(self.user_profile.columns):
             raise ValueError(
-                f'test_feature columns should be same as that used in fit(). Expected {list(self.user_profile.columns)}, got {list(test_features.columns)} instead')
+                f'test_item_profiles features should be same as that used in fit(). Expected {list(self.user_profile.columns)}, got {list(test_item_profiles.columns)} instead')
 
-        items = items.squeeze()
-        feature_matrix = test_features.values
+        items = test_items.squeeze()
+        feature_matrix = test_item_profiles.values
         expected_scores = (
             feature_matrix * self.user_profile.values).sum(axis=1) * 10
 
         recommendations = DataFrame({
-            'item': items,
-            'expected score': expected_scores}).sort_values('expected score', ascending=False)
-        return recommendations[recommendations['expected score'] >= threshold_score].iloc[:limit, :]
+            'Item': items,
+            'Expected Score': expected_scores}).sort_values('Expected Score', ascending=False)
+        return recommendations[recommendations['Expected Score'] >= min_score].iloc[:limit, :]
